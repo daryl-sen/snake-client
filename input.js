73198c1f2f2f8ea55cf3ce393b69a6f12c7703cf
daryl <tsdaryl@gmail.com>
2021-01-13 14:44:46 -0800

    added constants.js

--- input.js
+++ input.js
@@ -15,6 +15,21 @@ const handleUserInput = function(key) {
     case 'd':
       connection.write('Move: right');
       break;
+    case '1':
+      connection.write('Say: HA HA!');
+      break;
+    case '2':
+      connection.write('Say: Plash Speed 5');
+      break;
+    case '3':
+      connection.write('Say: Beep Beep');
+      break;
+    case '4':
+      connection.write('Say: Longer vertically');
+      break;
+    case '5':
+      connection.write('Say: Big snek');
+      break;
     case '\u0003':
       process.exit();
   }
