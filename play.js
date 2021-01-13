52de69fdbb5531aac0e661a3a93e7e71a4872553
daryl <tsdaryl@gmail.com>
2021-01-13 13:28:33 -0800

    separated input.js

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const connect = require('./client.js');
+const setupInput = require('./input.js');
 
 console.log('Connecting ...');
-
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', handleUserInput);
-  stdin.resume();
-  return stdin;
-};
-
 
 setupInput();
 
