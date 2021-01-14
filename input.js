7a378a2b10c170de5354123a106aafeda6b419a2
daryl <tsdaryl@gmail.com>
2021-01-13 18:36:21 -0800

    added autorunner to prevent disconnection

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
+const { clear } = require("console");
+
 // stores active TCP conncetion object
 let connection;
+let toggle = [false];
 
 const handleUserInput = function(key) {
   switch (key) {
@@ -33,6 +36,48 @@ const handleUserInput = function(key) {
     case '\u0003':
       process.exit();
   }
+
+  // press '/' to toggle the autorunner, then press '=' to start execution
+  if (key === '/') {
+    if (toggle[0] === true) {
+      toggle[0] = false;
+    } else {
+      toggle[0] = true;
+    }
+    if (toggle[0] === true) {
+      console.log('Autorunner is ready!! Press "=" to execute!');
+    } else {
+      console.log('Autorunner has been deactivated.');
+    }
+  }
+
+  // press '/' again to stop the autorunner
+  if (key === '=' && toggle) {
+    console.log('autorunning');
+
+    // one cycle of movements, feel free to change it!
+    const cycle = ['up', 'right', 'down', 'right'];
+    let cycleI = 0; // counts where we are in the cycle
+
+    let auto = setInterval(() => {
+      if (toggle[0] === true) {
+
+        connection.write('Move: ' + cycle[cycleI]);
+        connection.write('Say: Idling...');
+
+        if (cycleI < cycle.length) {
+          cycleI++; // move on to the next movement direction
+        } else {
+          cycleI = 0; // restart the cycle from the first movement direction
+        }
+
+      } else {
+        console.log('ended');
+        clearInterval(auto); // stop setInterval
+      }
+    }, 1000); // run every second, feel free to change
+  }
+
 };
 
 const setupInput = function(conn) {
